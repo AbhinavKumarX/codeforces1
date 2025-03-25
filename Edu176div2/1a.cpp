15ce9a02554cd6aa49ed8a42edc7cbd07372f9f4
Abhinav Kumar <abhi@Abhinavs-MacBook-Air.local>
2025-03-25 10:45:19 +0530

    before 1013 div 3

--- Edu176div2/1a.cpp
+++ Edu176div2/1a.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    int n;
-    std::cout << "Enter a number: ";
-    std::cin >> n;
-    std::cout << "You entered: " << n << std::endl;
-    return 0;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k, counter = 0;
+        cin >> n >> k;
+        while (n > k)
+        {
+            if (n % 2 == 0) // even
+            {
+                n = (n - (k - 1));
+            }
+            else
+            { // odd
+                n = (n - k);
+            }
+            counter++;
+        }
+        cout << counter + 1 << "\n";
+    }
 }
